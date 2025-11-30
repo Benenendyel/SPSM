e43363be2fdbcd67f323e81619b8e352e7e8fcc1
Benenendyel <Vencarlostagaro@gmail.com>
2025-11-30 20:47:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Supermarket Product Management System
+
+The **Supermarket Product Management System** is a locally-hosted, browser-based solution designed to streamline in-store operations. It provides a seamless experience for customers, cashiers, and store administrators through three dedicated interfaces:
+
+---
+
+# Supports three interfaces
+
+1. Customer Scanning Stations - can only view product details
+2. Cashier Terminal - can handle checkouts
+3. Administrator Dashboard - can do full CRUD
+
+---
+
+# Tools we used
+
+1. NodeJS
+2. JS (HTML, CSS)
+3. SQLLite - for portability of
+
+# Contributors
+
+1. Tagaro, Ven Angel C.
+2. Paulino, John Albert
+3. Antonio, Alvin M., Jr.
---
+++ server.js
@@ -0,0 +1,66 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const port = 3000;
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  const contentTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".gif": "image/gif",
+    ".svg": "image/svg+xml",
+    ".ico": "image/x-icon",
+  };
+  return contentTypes[extname] || "application/octet-stream";
+}
+
+const server = http.createServer(function (req, res) {
+  let filePath = "";
+
+  if (
+    req.url.startsWith("/assets/") ||
+    req.url.startsWith("/css/") ||
+    req.url.startsWith("/js/")
+  ) {
+    filePath = "./public" + req.url;
+  } else if (req.url === "/") {
+    filePath = "./public/index.html";
+  } else if (req.url === "/customer") {
+    filePath = "./public/customer.html";
+    console.log("Displaying: ", filePath);
+  } else if (req.url === "/cashier") {
+    filePath = "./public/cashier.html";
+    console.log("Displaying:", filePath);
+  } else if (req.url === "/admin") {
+    filePath = "./public/admin.html";
+    console.log("Displaying:", filePath);
+  } else {
+    filePath = "./public/404.html";
+    console.log("Displaying:", filePath);
+  }
+
+  fs.readFile(filePath, function (error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("Error: FIle not found ");
+    } else {
+      const contentType = getContentType(filePath);
+      res.writeHead(200, { "Content-Type": contentType });
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+server.listen(port, function (error) {
+  if (error) {
+    console.log("There was an error", error);
+  } else {
+    console.log("Listening to port", port);
+  }
+});
